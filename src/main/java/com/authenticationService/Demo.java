2e646435c08efecdc257254f520a0a61c3831b4c
raushankumarmishra <114497614+raushankumarmishra@users.noreply.github.com>
2025-08-12 19:36:24 +0530

    Created Demo.java in authenticationService folder to check the workflow

---
+++ src/main/java/com/authenticationService/Demo.java
@@ -0,0 +1,11 @@
+package com.authenticationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
